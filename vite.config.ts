e86034a8d49acab7d82718065b9ed6268ccda544
sonamchokss <chokipromax@gmail.com>
2025-07-23 20:09:22 +0600

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/chokiss-work/',
+  base: "/chokiss-work/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    // mode === 'development' && componentTagger(), // Uncomment if you use this plugin
   ].filter(Boolean),
   resolve: {
     alias: {
